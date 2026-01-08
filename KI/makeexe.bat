1dc044eacb440da042b85f420d5d11e752f77007
arduinodude456 <onlyformusic456@outlook.de>
2026-01-08 08:55:39 +0100

    Update makeexe.bat

--- KI/makeexe.bat
+++ KI/makeexe.bat
@@ -1,9 +1,11 @@
 pip install pyinstaller
+pip install requests
 pip install pillow
 pip install groq
 pyinstaller --onefile --noconsole ^
   --add-data "cat.jpg;." ^
   --add-data "wissen.txt;." ^
   lingo.py
+
 
 
