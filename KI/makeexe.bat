3a692c5adec267a13b5bf11bbd11f346d626f3b7
arduinodude456 <onlyformusic456@outlook.de>
2026-01-08 09:05:43 +0100

    Update makeexe.bat

--- KI/makeexe.bat
+++ KI/makeexe.bat
@@ -2,9 +2,11 @@ py -m pip install pyinstaller
 py -m pip install requests
 py -m pip install pillow
 py -m pip install groq
+py -m pip install threading
 py -m pyinstaller --onefile --noconsole ^
   --add-data "cat.jpg;." ^
   --add-data "wissen.txt;." ^
-  lingo.py
+  assistant.py
+
 
 
